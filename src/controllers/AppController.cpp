1efdbdb59d8ab7d8015dd9b46686e78310c191b0
Krzysztof Urbanowicz <terhon95@gmail.com>
2018-12-02 21:57:32 +0100

    Wrzucenie do folderow i glowny loop

---
+++ src/controllers/AppController.cpp
@@ -0,0 +1,10 @@
+#include <AppController.hpp>
+
+AppController::AppController(App* app) : app(app) 
+{}
+
+void AppController::handleEvents(sf::Event event) 
+{
+    if (event.type == sf::Event::Closed)
+        app->setRunning(false);
+}
