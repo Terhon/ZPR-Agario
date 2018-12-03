ae4a0d6b835106ec83ebbc79117b37200f009b05
Krzysztof Urbanowicz <terhon95@gmail.com>
2018-12-03 21:09:34 +0100

    kilka plikow uhu

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,7 @@
-#define BOOST_TEST_MODULE const_string test
 #define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE main
 #include <boost/test/unit_test.hpp>
-#include "App.hpp"
 
-BOOST_AUTO_TEST_CASE(dummy) {
+BOOST_AUTO_TEST_CASE(whatisthis) {
     BOOST_CHECK(1 + 1 == 2);
 }
---
+++ tests/models/AppTest.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "AppTest"
+#include <boost/test/unit_test.hpp>
+
+#include <App.hpp>
+
+BOOST_AUTO_TEST_CASE(why)
+{
+      BOOST_CHECK(1 + 1 == 2);
+}
