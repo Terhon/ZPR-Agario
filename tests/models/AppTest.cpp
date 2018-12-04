93fc7b48ed841dae6318b293b87a6bf0e7cac067
Krzysztof Urbanowicz <terhon95@gmail.com>
2018-12-04 09:45:35 +0100

    fixed tests

--- tests/models/AppTest.cpp
+++ tests/models/AppTest.cpp
@@ -4,8 +4,31 @@
 
 #include <App.hpp>
 
-BOOST_AUTO_TEST_CASE(why)
-{
-  App app;
-  app.getWindow();
+struct AppFixture {
+    AppFixture() { 
+        app = new App();
+        BOOST_TEST_MESSAGE( "setup AppFixture" ); 
+    }
+    ~AppFixture() { 
+        BOOST_TEST_MESSAGE( "teardown AppFixture" ); 
+        delete app;
+    }
+
+    App * app;
+};
+/*
+BOOST_AUTO_TEST_CASE(peek_returns_null_on_new_app){
+    AppFixture f;
+    BOOST_CHECK(f.app->peekStack() == nullptr);
+}*/
+
+BOOST_AUTO_TEST_CASE(new_app_apps_stacks_size_is_zero){
+    AppFixture f;
+    BOOST_CHECK(f.app->stackSize() == 0);
+}
+
+BOOST_AUTO_TEST_CASE(after_first_update_stack_size_equals_one){
+    AppFixture f;
+    f.app->update();
+    BOOST_CHECK(f.app->stackSize() == 1);
 }
