8d7190417c034ac648640d445adefdb3fd308438
Terhon <terhon95@gmail.com>
2019-01-15 22:21:35 +0100

    testy App

--- tests/models/AppTest.cpp
+++ tests/models/AppTest.cpp
@@ -4,6 +4,7 @@
 
 #include <App.hpp>
 #include <states/StartModel.hpp>
+#include <states/PauseModel.hpp>
 
 struct AppFixture {
     AppFixture() { 
@@ -25,5 +26,28 @@ BOOST_FIXTURE_TEST_CASE(push_stack_increases_stack_size, AppFixture){
 
 BOOST_FIXTURE_TEST_CASE(new_app_apps_stacks_size_is_zero, AppFixture){
     BOOST_CHECK(app->stackSize() == 0);
+}
+
+BOOST_FIXTURE_TEST_CASE(set_running_false_ends_app, AppFixture)
+{
+    app->setRunning(false);
+    app->run();
+    BOOST_CHECK(true);
+}
+
+BOOST_FIXTURE_TEST_CASE(pop_stack_decreases_stack_size, AppFixture)
+{
+    app->pushStack(new StartModel(app));
+    app->popStack();
+    BOOST_CHECK(app->stackSize() == 0);
+}
+
+BOOST_FIXTURE_TEST_CASE(peek_stack_shows_top_state, AppFixture)
+{
+    app->pushStack(new StartModel(app));
+    app->pushStack(new PauseModel(app, 0));
+    BOOST_CHECK(dynamic_cast<PauseModel*>(app->peekStack()));
+    app->popStack();
+    BOOST_CHECK(dynamic_cast<StartModel*>(app->peekStack()));
 }
 
